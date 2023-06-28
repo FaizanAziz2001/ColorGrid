45ca2228657a922348d55ef757982fb54693914c
Faizan <faizanaziz2001@gmail.com>
2023-06-28 20:34:33 +0500

    Fixed Some Bugs

--- script.js
+++ script.js
@@ -1,60 +1,71 @@
-let size=64;
-  
+let size = 64;
+let current_color = 'black';
+let mouseTrigger = false;
 
-const setEvents=()=>
-{
-    const button=document.querySelector('#inner-button');
-    button.addEventListener('click',loadGrid);
+const setEvents = () => {
+    const setbtn = document.querySelector('#inner-button');
+    const clearbtn=document.querySelector('#clear-btn');
+    const erasebtn=document.querySelector('#erasor');
+
+    setbtn.addEventListener('click', loadGrid);
+    clearbtn.addEventListener('click',loadGrid);
+    erasebtn.addEventListener('click',()=>{current_color='white'});
 }
 
-const setSliderText=()=>
-{
-    const text=document.querySelector('#inner-text');
-    const sliderVal=document.querySelector('#inner-slider');
-
-    text.textContent=sliderVal.value;
+const changeColor = (e) => {
+    if (e.type == "mouseover" && mouseTrigger == true)
+        e.target.style.backgroundColor = current_color;
+    else if (e.type == "mousedown" && mouseTrigger == false)
+        mouseTrigger = true;
+    else if (e.type == "mousedown" && mouseTrigger == true)
+        mouseTrigger = false;
 }
-const createBlock=()=>{
-    let div=document.createElement('div');
-    let parent=document.querySelector('#inner-grid');
+const setSliderText = (value) => {
+    const text = document.querySelector('#inner-text');
+    text.textContent = value;
+}
+const createBlock = () => {
+    let div = document.createElement('div');
+    let parent = document.querySelector('#inner-grid');
 
-    div.className="cell";
-    div.style.backgroundColor="white";
-    div.addEventListener("mouseover",(e)=>
-    {   
-        
-    })
+    div.className = "cell";
+    div.addEventListener("mouseover", changeColor);
+    div.addEventListener("mousedown", changeColor);
 
 
     parent.appendChild(div);
 }
 
+const updateColor = (value) => {
+    current_color = value;
+}
 
 
-const loadGrid=()=>
-{
-    const bodycontainer=document.querySelector('.inner-body');
-    const deletegrid=document.querySelector('#inner-grid');
-    if(deletegrid)
-    {
+
+const loadGrid = () => {
+    const bodycontainer = document.querySelector('.inner-body');
+    const deletegrid = document.querySelector('#inner-grid');
+    if (deletegrid) {
         bodycontainer.lastChild.remove();
     }
 
-    const gridcontainer=document.createElement('div');
-    const slider=document.querySelector("#inner-slider");
+    const gridcontainer = document.createElement('div');
+    const slider = document.querySelector("#inner-slider");
 
 
-    const size=slider.value;
-    gridcontainer.id="inner-grid";
-    gridcontainer.style.gridTemplateRows=`repeat(${size},1fr)`;
-    gridcontainer.style.gridTemplateColumns=`repeat(${size},1fr)`;
+    const size = slider.value;
+    gridcontainer.id = "inner-grid";
+    gridcontainer.style.gridTemplateRows = `repeat(${size},1fr)`;
+    gridcontainer.style.gridTemplateColumns = `repeat(${size},1fr)`;
     bodycontainer.appendChild(gridcontainer);
 
-    for(let i=0;i<size*size;i++)
-    {
+    for (let i = 0; i < size * size; i++) {
         createBlock();
     }
 }
 
-loadGrid();
-setEvents();
+window.onload = () => {
+    loadGrid();
+    setEvents();
+
+}
